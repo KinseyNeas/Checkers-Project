16e3a27dbcc6072782b2c403b7d8a3e98e12d06b
mmorale4 <90280791+mmorale4@users.noreply.github.com>
2022-11-15 19:04:28 -0600

    Updated solver.hs
    Added necessary functions

--- Solver.hs
+++ Solver.hs
@@ -1,15 +1,36 @@
-module Solver
--- ( whoWins
--- , bestMove
--- )
-where
+module Main where
 import Checkers
+import Data.List.Split
+--playCheckers :: GameState -> IO()
+--playCheckers game = do
+--    if not (checkGameOver game)
+--       then continueGame
+--    else endGame where
+--    continueGame = undefined
+--    endGame = undefined
 
-data Outcome = RedWin | BlackWin | Tie
+readGame :: String -> Maybe GameState -- converts file String to GameState
+readGame a = let (x:xs) = splitOn "| " a
+                  col = zip [0..7] splitOn "\n" x
+                  board = makeBoard splitOn "  " col
+             in (color, board, maybeLoc, count)
 
-whoWillWin :: GameState -> Outcome
-whoWillWin gs@(c, board, mLoc, ct) = 
+makeBoard :: String -> Maybe Board
+makeBoard (x:xs) = 
+makeBoard [] = Nothing
 
-bestMove :: GameState -> Move
-bestMove 
+--     case zip [0 .. 7] (splitOn "," shroom) of
+--        (x:xs) -> sequence (catMaybes ((map observationWord xs)), observationNom x) -- Return the Observation... maybe
+ --       [] -> Nothing
 
+showGame :: GameState  -> String -- converts GameState to file String
+showGame = undefined
+
+writeGame :: GameState -> FilePath -> IO () -- showGame() to IO() [Writes the game]
+writeGame = undefined
+
+loadGame :: FilePath -> IO GameState -- readGame() to get the gamestate and show in IO
+loadGame = undefined
+
+putWinner :: Game -> IO () -- Hold on this one for the other group
+putWinner = undefined
