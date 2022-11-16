bae0bd84706aecfecdffdf2ce09053abbc403c66
Joshua Gammon <joshuagammon@joshuas-air.trinity.local>
2022-11-16 11:44:47 -0600

    Finished whoWillWin but its very dependent on bestMove

--- Solver.hs
+++ Solver.hs
@@ -1,14 +1,17 @@
-module Solver
+module Solver where
 -- ( whoWins
 -- , bestMove
 -- )
-where
 import Checkers
 
-data Outcome = RedWin | BlackWin | Tie
-
 whoWillWin :: GameState -> Outcome
-whoWillWin gs@(c, board, mLoc, ct) = 
+whoWillWin gs@(c, board, mLoc, ct) =
+    case gameStatus gs of
+        Nothing -> whoWillWin newGS
+        Just RedWin -> RedWin
+        Just BlackWin -> BlackWin
+    where Just newGS = makeMove gs (bestMove gs)
+    --ask about unsafe pattern matching (Just newGS)
 
 bestMove :: GameState -> Move
-bestMove 
+bestMove = undefined --validMoves
