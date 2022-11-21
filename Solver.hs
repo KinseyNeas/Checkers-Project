bcc540dca29f8ac842273c8b8f8d17cdc44585e2
Joshua Gammon <joshuagammon@Joshuas-Air.lan1>
2022-11-20 19:33:48 -0600

    Removed bestMove notes. It done.

--- Solver.hs
+++ Solver.hs
@@ -6,14 +6,6 @@ import Data.Maybe
 import Checkers
 import TesterCode
 
---whoWillWin :: GameState -> Outcome
---whoWillWin gs@(c, board, mLoc, ct) =
---    case gameStatus gs of
---        Just x -> x
---        Nothing -> whoWillWin newGS
---    where Just newGS = makeMove gs (bestMove gs)
-    --ask about unsafe pattern matching (Just newGS)
---data Tree = Node [Tree] | Leaf Outcome deriving Show
 
 whoWillWin :: GameState -> Outcome
 whoWillWin gs@(c, board, mLoc, ct) = 
@@ -36,15 +28,7 @@ decideWinner lst col
                     Win Red -> Win Black
                     Win Black -> Win Red
 
--- psuedocode
-
--- gameStatus gs
--- validMoves gs -> list of valid moves
--- for each validMove, call makeMove -> list of game states
--- needs to go through each possible situation until it finds a match
-
 bestMove :: GameState -> Move
-<<<<<<< HEAD
 bestMove gs@(c, board, mLoc, ct) = case gameStatus gs of 
             Nothing -> let validMovesLst = validMoves gs
                            gsLst = catMaybes $ map (makeMove gs) validMovesLst
@@ -69,22 +53,4 @@ getTuple lst@(l:ls) c = case [(mv, otc) | (mv, otc) <- lst, otc == Win c] of
                                 (y:ys) -> y
                                 [] -> l
                 
-
---bestMoveHelp move gs = case gameStatus gs of
---             Nothing -> (move, outcome)
---             Just x = let validMoveLst = validMoves gs
---                          newGsLst = map --(\x -> (move, makeMove x gs))-- validMoveLst -- just map make move
---                          outcomeLst = map (\(x,y) -> (x, bestMoveHelp x y))  - just call best help on all
---                        in deriveOutcome&Move outcomeLst
-
--- deriveOutcome&Move :: [(move, outcome)] -> Color -> (move,outcome)
--- deriveOutcome&Move lst@(x:xs) col = case [(x,y)|(x,y)<- lst, y == Win col] OR filter (\(x,y) -> y /= Win !Color ) lst of
---    (y:ys) -> y
---    [] -> case [(x,y)|(x,y)<- lst, y == Tie] of
---                (f:fs) -> f
---                [] -> x
-
---checkTie :: [()] -> Bool
---checkPlayer :: Color -> [()] -> Bool
---checkPlayer col lst = foldr (\(x,y) retVal -> y == Win col || )
 
